bddc39a575ea425831493e5d1105898b3712f828
Hiranya Mendis <hiranyamendis52@gmail.com>
2025-11-21 22:04:39 +0530

    Design dashboard

--- order_management_system/src/main/java/controller/DashboardController.java
+++ order_management_system/src/main/java/controller/DashboardController.java
@@ -1,4 +1,28 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class DashboardController {
+
+    @FXML
+    void btnCustomerInfoOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnItemInfoOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOrderDetailsOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOrdersInfoOnAction(ActionEvent event) {
+
+    }
+
 }
