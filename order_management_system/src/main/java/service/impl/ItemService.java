f3ff4e285e4b500110153c31c4a3287bfd9f04c2
Hiranya Mendis <hiranyamendis52@gmail.com>
2025-11-23 00:48:41 +0530

    Set up ItemService interface structure

--- order_management_system/src/main/java/service/impl/ItemService.java
+++ order_management_system/src/main/java/service/impl/ItemService.java
@@ -1,4 +1,16 @@
 package service.impl;
 
+import javafx.collections.ObservableList;
+import model.dto.ItemDTO;
+
 public interface ItemService {
+
+    void addItemDetails(String itemCode, String description, String PackSize, double unitPrice, int qtyOnHand);
+
+    void deleteItemDetails(String itemCode);
+
+    void updateItemDetails(String itemCode, String description, String PackSize, double unitPrice, int qtyOnHand);
+
+    ObservableList<ItemDTO> getAllItemDetails();
+
 }
